001d71753e84b9588a80f3949dd1c751d9e7c5b6
yakdd <yakdd@yandex.ru>
2024-03-18 00:29:49 +0300

    fix_3

--- recipesite/settings.py
+++ recipesite/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'media/']
+# STATICFILES_DIRS = [BASE_DIR / 'media/']
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
